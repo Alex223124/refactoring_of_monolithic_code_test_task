e25025365aeda128a3b42711ce5501366e9966bc
Al <sologoubalex@gmail.com>
2018-04-11 22:17:14 +0300

    String class refactoring: added comment for method and warning about class reopening

--- lib/ruby_std_lib_extensions/string.rb
+++ lib/ruby_std_lib_extensions/string.rb
@@ -1,5 +1,8 @@
 class String
 
+  puts "Warning! String class has extensions!"
+
+  # converts a string to a float type, replacing the comma with a period for the correct type conversion
   def from_german_to_f
     self.gsub(',', '.').to_f
   end
