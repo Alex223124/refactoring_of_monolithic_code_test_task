d2791c2f5071d0de2020e4ce12e79fd019d3cdbe
Al <sologoubalex@gmail.com>
2018-04-11 22:12:56 +0300

    Float class refactoring: added comment for method and warning about class reopening

--- lib/ruby_std_lib_extensions/float.rb
+++ lib/ruby_std_lib_extensions/float.rb
@@ -1,5 +1,8 @@
 class Float
 
+  puts "Warning! Float class has extensions!"
+
+  # converts the float value to a string with the same value and replaces the point with a floating point
   def to_german_s
     self.to_s.gsub('.', ',')
   end
