b9d0f23dea3ff94cd37f3568548f4389e5e5a9d7
Al <sologoubalex@gmail.com>
2018-04-11 19:55:20 +0300

    files reorganised in more modulear way

--- lib/ruby_std_lib_extensions/csv_operations.rb
+++ lib/ruby_std_lib_extensions/csv_operations.rb
@@ -1,13 +1,21 @@
 class CSVOperations
 
+  # (+)
   LINES_PER_FILE = 120000.freeze
+  # (+)
   DEFAULT_CSV_OPTIONS = { :col_sep => "\t", :headers => :first_row }.freeze
+  # (+)
   DEFAULT_CSV_WRITE_OPTIONS = DEFAULT_CSV_OPTIONS.merge(:row_sep => "\r\n")
 
+  #parses data from file
+  # (+)
   def self.parse(file)
+    binding.pry
     CSV.read(file, DEFAULT_CSV_OPTIONS)
   end
 
+  #reads rows from file and then generates an enumerator
+  # (+)
   def self.lazy_read(file)
     Enumerator.new do |yielder|
       CSV.foreach(file, DEFAULT_CSV_OPTIONS) do |row|
@@ -16,6 +24,10 @@ class CSVOperations
     end
   end
 
+  # изменить названия этих файлов, из названия не понятно что делат один и что делает второй
+  # write_content_to_csv
+  # saves content to csv file
+  # (+)
   def self.write(content, headers, output)
     CSV.open(output, "wb", DEFAULT_CSV_WRITE_OPTIONS) do |csv|
       csv << headers
@@ -25,6 +37,7 @@ class CSVOperations
     end
   end
 
+  # (+)
   def self.write_to_csv(merger, output)
     done = false
     file_index = 0
@@ -56,6 +69,7 @@ class CSVOperations
   end
 
 
+  # (+)
   def self.remove_file_extension_from_(path, file_extension = '.txt')
     path.gsub(file_extension, '')
   end
