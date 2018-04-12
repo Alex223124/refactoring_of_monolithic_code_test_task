1c0c48326d5cf6cb59d872282227a6ac37d435ec
Al <sologoubalex@gmail.com>
2018-04-12 19:32:41 +0300

    comments to RowsToHashService added

--- lib/services/rows_to_hash_service.rb
+++ lib/services/rows_to_hash_service.rb
@@ -1,15 +1,18 @@
 class RowsToHashServcie
 
+  #initializes an instance variable for further use
   def initialize(array_of_lists)
     @array_of_lists = array_of_lists
   end
 
+  # runs service logic
   def run
     format_hash_values(combine_hash)
   end
 
   private
 
+  # creates single hash from csv data
   def combine_hash
     merged_hashes = {}
     @array_of_lists.each do |list_of_rows|
@@ -19,6 +22,7 @@ class RowsToHashServcie
     merged_hashes
   end
 
+  # covnerts rows from csv to hash, validating blank rows
   def convert_rows_to_hash(list_of_rows)
     hash = {}
     list_of_rows.each do |row|
@@ -28,16 +32,17 @@ class RowsToHashServcie
     hash
   end
 
+  # merges hash values which has same key
   def merge_hashes(h1, h2)
     h1.merge(h2){|k,v1,v2|[v1,v2]}
   end
 
+  # formats hash values from nested arrays to regular arrays
   def format_hash_values(hash)
     hash.each do |key, value|
       hash[key] = value.flatten
     end
     hash
   end
-
 
 end
