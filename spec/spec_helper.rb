be14f3ab7e0f933769af8b34bc07f1b6b2db99a3
Al <sologoubalex@gmail.com>
2018-04-12 20:33:08 +0300

    paths for all files added for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
 require 'date'
 require 'pry'
+
 require './lib/parsers/file_input_parser'
+require './lib/parsers/command_line_arguments_parser'
+
+require './lib/ruby_std_lib_extensions/csv_operations'
+require './lib/ruby_std_lib_extensions/float'
+require './lib/ruby_std_lib_extensions/string'
+
+require './lib/services/report_recalculation_service'
+require './lib/services/rows_to_hash_service'
