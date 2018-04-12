59da0f4dd18ad3417581173fc1977477235da6c3
Al <sologoubalex@gmail.com>
2018-04-12 15:24:50 +0300

    Modifier class: methods refactored and comments added

--- modifier.rb
+++ modifier.rb
@@ -15,21 +15,19 @@ class Modifier
 	LAST_REAL_VALUE_WINS = ['Last Avg CPC', 'Last Avg Pos'].freeze
 
 	# initializes factors fr future usage in sorting
-	def initialize(sale_amount_factor, cancellation_factor)
+	def initialize(input, sale_amount_factor, cancellation_factor)
 		@sale_amount_factor = sale_amount_factor
 		@cancellation_factor = cancellation_factor
+		@input = input
 	end
 
 	# uses a set of methods for the report's recalculation
-	def modify(input)
-		input = sort(input)
-		array_of_lists = CSVOperations.to_array_converter(input)
-		merged_hashes = RowsToHashServcie.new(array_of_lists).run
-		recalculated_hash = combine_values_for_(merged_hashes)
-		CSVOperations.hash_to_csv(recalculated_hash, input)
+	def modify
+		input = sort(@input)
+		recalculate(input)
 	end
 
-	# sorts the data for the subsequent recalculation of the values of certain indicators
+	# sorts the data from csv
 	def sort(file)
 		output = "#{file}.sorted"
 		content_as_table = CSVOperations.parse(file)
@@ -38,6 +36,15 @@ class Modifier
 		content = content_as_table.sort_by { |a| -a[index_of_key].to_i }
 		CSVOperations.write(content, headers, output)
 		output
+	end
+
+	# recalculates the data from sorted csv and selects
+	# some data by conditions from ReportRecalculationService service
+	def recalculate(input)
+		array_of_lists = CSVOperations.to_array_converter(input)
+		merged_hashes = RowsToHashServcie.new(array_of_lists).run
+		recalculated_hash = combine_values_for_(merged_hashes)
+		CSVOperations.hash_to_csv(recalculated_hash, input)
 	end
 
 	private
