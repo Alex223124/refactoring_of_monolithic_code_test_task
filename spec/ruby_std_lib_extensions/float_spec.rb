147cc268b44277bc25a3e04bcd0e1f95dcd02ca6
Al <sologoubalex@gmail.com>
2018-04-13 14:09:22 +0300

    tests for Float class: wrong variable name renamed

--- spec/ruby_std_lib_extensions/float_spec.rb
+++ spec/ruby_std_lib_extensions/float_spec.rb
@@ -4,17 +4,17 @@ describe "float" do
 
   describe "#to_german_s" do
 
-    float_inside_the_stirng = 1.1
+    float_number = 1.1
 
     it "should convert float value to a string" do
-      expect(float_inside_the_stirng).to be_a(Float)
-      expect(float_inside_the_stirng.to_german_s).to be_a(String)
+      expect(float_number).to be_a(Float)
+      expect(float_number.to_german_s).to be_a(String)
     end
 
     it "should replace the point with a floating point" do
-      expect(float_inside_the_stirng.to_s.include?(".")).to eq(true)
-      expect(float_inside_the_stirng.to_german_s.include?(".")).to eq(false)
-      expect(float_inside_the_stirng.to_german_s.include?(",")).to eq(true)
+      expect(float_number.to_s.include?(".")).to eq(true)
+      expect(float_number.to_german_s.include?(".")).to eq(false)
+      expect(float_number.to_german_s.include?(",")).to eq(true)
     end
 
   end
